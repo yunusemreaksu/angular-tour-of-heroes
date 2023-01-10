c59cd7553b3096ec8f6d465d398cf65454745f4d
Yunus Emre Aksu <emre_aksu@live.com>
2023-01-10 10:31:38 +0300

    Added observable data logic

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -18,7 +18,7 @@ export class HeroesComponent {
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 
   ngOnInit(): void {
