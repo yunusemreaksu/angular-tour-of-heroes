fd5bcd7e96dfe87e56341cea4ad208f12f672e23
Yunus Emre Aksu <emre_aksu@live.com>
2023-01-11 10:49:09 +0300

    Added new hero adding feature

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -66,4 +66,11 @@ export class HeroService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -19,4 +19,14 @@ export class HeroesComponent {
   ngOnInit(): void {
     this.getHeroes();
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService
+      .addHero({ name } as Hero)
+      .subscribe((hero) => this.heroes.push(hero));
+  }
 }
